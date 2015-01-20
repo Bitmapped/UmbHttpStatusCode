2aa19c9877c5ef26ff9e7745fa506ab3713125ac
Bitmapped_cp <SND\Bitmapped_cp>
2015-01-20 18:43:40 +0000

    Added debugging.  Imporved interaction with other plugins.

--- UmbHttpStatusCode/UmbHttpStatusCode/Events/HttpStatusCodeEventHandler.cs
+++ UmbHttpStatusCode/UmbHttpStatusCode/Events/HttpStatusCodeEventHandler.cs
@@ -1,87 +1,91 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
+using Umbraco.Core.Configuration;
 using Umbraco.Core;
-using umbraco;
-using umbraco.cms.businesslogic.web;
-using umbraco.NodeFactory;
+using Umbraco.Core.Logging;
+using Umbraco.Web.Routing;
 
 namespace UmbHttpStatusCode.Events
 {
-    public class HttpStatusCodeEventHandler : IApplicationEventHandler
+    public class HttpStatusCodeEventHandler : ApplicationEventHandler
     {
-        // Ensure event handler is only registered once.
-        private static object registerLock = new object();
-        private static bool registerRan = false;
-
-        public void OnApplicationStarted(UmbracoApplicationBase httpApplicationBase, ApplicationContext applicationContext)
+        /// <summary>
+        /// Register event handler on start.
+        /// </summary>
+        /// <param name="httpApplicationBase">Umbraco application.</param>
+        /// <param name="applicationContext">Application context.</param>
+        protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {
-            // Handle locking.
-            if (!registerRan)
-            {
-                lock (registerLock)
-                {
-                    if (!registerRan)
-                    {
-                        // Register event.
-                        UmbracoDefault.AfterRequestInit += new UmbracoDefault.RequestInitEventHandler(this.SetHttpStatusCode);
-
-                        // Record that registration happened.
-                        registerRan = true;
-                    }
-                }
-            }
+            PublishedContentRequest.Prepared += PublishedContentRequest_Prepared;
         }
-
-        #region Unused interface methods
-        public void OnApplicationStarting(UmbracoApplicationBase httpApplicationBase, ApplicationContext applicationContext) { }
-        public void OnApplicationInitialized(UmbracoApplicationBase httpApplicationBase, ApplicationContext applicationContext) { }
-        #endregion
 
         /// <summary>
         /// Sets Http status code to value stored in umbHttpStatusCode property.
         /// </summary>
         /// <param name="sender">Sender object</param>
         /// <param name="e">Event properties</param>
-        private void SetHttpStatusCode(object sender, RequestInitEventArgs e)
+        private void PublishedContentRequest_Prepared(object sender, EventArgs e)
         {
-            // Ensure there is a page to load.
-            if (e.Page == null)
+            // Get request.
+            PublishedContentRequest request = sender as PublishedContentRequest;
+            HttpContext context = HttpContext.Current;
+
+            // Ensure request is valid and page exists.  Otherwise, return without doing anything.
+            if ((request == null) || (request.Is404) || (request.IsRedirect))
             {
-                // No valid page.  Do nothing.
+                // Log for debugging.
+                LogHelper.Debug<HttpStatusCodeEventHandler>("Stopping HttpStatusCode for requested URL {0} because request was null ({1}), was 404 ({2}), or was a redirect ({3}).",
+                    () => context.Request.Url.AbsolutePath,
+                    () => (request == null),
+                    () => (request.Is404),
+                    () => (request.IsRedirect));
+
                 return;
             }
 
-            // Load document corresponding with current page.
-            var node = new Node(e.PageId);
-
-            // Set status if umbHttpStatusCode property exists.
-            if (node.HasProperty("umbHttpStatusCode"))
+            // Determine if page has umbStatusCode property set.
+            if (request.PublishedContent.GetProperty("umbHttpStatusCode") != null)
             {
                 // Define variables for storing values.
                 int statusCode = 0;
-                var statusProperty = node.GetProperty("umbHttpStatusCode").Value.ToString();
+                int subStatusCode = 0;
+                var statusProperty = request.PublishedContent.GetProperty("umbHttpStatusCode").Value.ToString();
 
-                if (Int32.TryParse(statusProperty, out statusCode))
+                // Try to parse status code.  If valid and greater than 0, set response code.
+                if (Int32.TryParse(statusProperty, out statusCode) && (statusCode > 0))
                 {
-                    // Set status code.
-                    e.Context.Response.StatusCode = statusCode;
+                    // Set status code for IIS.
+                    context.Response.StatusCode = statusCode;
+
+                    // Set status code for Umbraco.
+                    request.SetResponseStatus(statusCode);
+
+                    // Try skipping custom IIS errors if desired.
+                    context.Response.TrySkipIisCustomErrors = UmbracoConfig.For.UmbracoSettings().WebRouting.TrySkipIisCustomErrors;
 
                     // Try setting substatus code if property exists.
-                    if (node.HasProperty("umbHttpSubStatusCode"))
+                    if (request.PublishedContent.GetProperty("umbHttpSubStatusCode") != null)
                     {
-                         // Define variables for storing values.
-                        int subStatusCode = 0;
-                        var subStatusProperty = node.GetProperty("umbHttpSubStatusCode").Value.ToString();
+                        // Define variables for storing values.
+                        var subStatusProperty = request.PublishedContent.GetProperty("umbHttpSubStatusCode").Value.ToString();
 
-                        if (Int32.TryParse(subStatusProperty, out subStatusCode))
+                        // Try to parse substatus code.  If valid and greater than 0, set response code.
+                        if (Int32.TryParse(subStatusProperty, out subStatusCode) && (subStatusCode > 0))
                         {
-                            // Set status code.
-                            e.Context.Response.SubStatusCode = subStatusCode;
+                            try
+                            {
+                                // Set status code.
+                                context.Response.SubStatusCode = subStatusCode;
+                            }
+                            catch { }
                         }
                     }
+
+                    // Log for debugger.
+                    LogHelper.Debug<HttpStatusCodeEventHandler>("Setting HTTP Status Code {0}.{1} for {2}.",
+                        () => statusCode,
+                        () => subStatusCode,
+                        () => context.Request.Url.AbsolutePath);
                 }
             }
         }
